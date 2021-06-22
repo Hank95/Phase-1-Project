adf7896313c1a189a5301ce1b12cd465eec8f8f3
github username <github email address>
2021-06-22 17:54:54 -0400

    added myList functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = "5dd59b3fb5f3ddffe05ee3a3306f88ff";
-
+let logInID = 1;
 fetch(
   `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=vote_average.desc`
 )
@@ -47,48 +47,34 @@ function renderMovie(movie) {
   const listButton = document.createElement("button");
   listButton.textContent = "Add to Watch List";
   listButton.className = "list-button";
+  listButton.type = "button";
   listButtonDiv.append(listButton);
   actionDiv.append(listButtonDiv);
-  console.log(listButton)
+
   container.append(divImage, divDetails, actionDiv);
 
   content.append(container);
 
   listButton.addEventListener("click", (e) => {
-    addToList(movie, e);
+    e.preventDefault();
+    addToList(movie, e, logInID);
   });
 }
 
-function addToList(movie, e) {
+function addToList(movie, e, logInID) {
   console.log(movie);
   console.log(e);
-  fetch(`http://localhost:3000/profile/1`)
+  fetch(`http://localhost:3000/profile/${logInID}`)
     .then((res) => res.json())
     .then((json) => {
       const list = json.myList;
-<<<<<<< HEAD
-      console.log(list);
-    });
-
-  // let movieData = movie;
-  // fetch(`http://localhost:3000/myList`, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify(movieData),
-  // })
-  //   .then((res) => res.json())
-  //   .then((json) => console.log(json));
-}
-
-function addToMyListArr(json) {
-  const list = json.myList;
-  console.log(list);
-=======
+      const movieWDate = movie;
+      let today = new Date().toLocaleDateString();
+      movieWDate.dateAdded = today;
+      console.log(movieWDate);
       list.push(movie);
       console.log(list);
-      fetch(`http://localhost:3000/profile/1`, {
+      fetch(`http://localhost:3000/profile/${logInID}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
@@ -98,7 +84,6 @@ function addToMyListArr(json) {
         }),
       });
     });
->>>>>>> 4355e92a74931baeea3ac7d0d52431bbd87b5064
 }
 
 const comments = document.querySelector(".comments");
@@ -118,45 +103,45 @@ const searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   content.innerHTML = "";
+  listContainer.innerHTML = "";
 
   console.log(e.target.search.value);
   findMovie(e.target.search.value);
 });
 
-<<<<<<< HEAD
-listButton.addEventListener("click", (e) => {
-  addToList(movie, e);
-});
-
-
-function addToList(movie, e) {
-console.log(movie);
-console.log(e);
-fetch(`http://localhost:3000/profile/1`)
-  .then((res) => res.json())
-  .then((json) => {
-    const list = json.myList;
-    console.log(list);
-  });
-
-// let movieData = movie;
-// fetch(`http://localhost:3000/myList`, {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify(movieData),
-// })
-//   .then((res) => res.json())
-//   .then((json) => console.log(json));
-}
-
-function addToMyListArr(json) {
-const list = json.myList;
-console.log(list);
-}
-=======
 const theList = document.querySelector(".my-list");
 
-theList.addEventListener("click", e);
->>>>>>> 4355e92a74931baeea3ac7d0d52431bbd87b5064
+theList.addEventListener("click", (e) => {
+  content.innerHTML = "";
+  renderMyList(logInID);
+});
+
+function renderMyList(logInID) {
+  fetch(`http://localhost:3000/profile/${logInID}`)
+    .then((res) => res.json())
+    .then((json) => json.myList.forEach((movie) => movieCard(movie)));
+}
+
+const listContainer = document.querySelector("#list-container");
+function movieCard(movie) {
+  const movieDiv = document.createElement("div");
+  movieDiv.className = "movie-div";
+  const img = document.createElement("img");
+  img.src = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+  const movieInfoDiv = document.createElement("div");
+  movieInfoDiv.className = "movie-info";
+  const h3 = document.createElement("h3");
+  const dateP = document.createElement("p");
+  const ratingP = document.createElement("p");
+  const watchedP = document.createElement("p");
+  h3.textContent = movie.original_title;
+  dateP.textContent = movie.dateAdded;
+  ratingP.textContent = `${movie.vote_average}/10`;
+  watchedP.textContent = "Watched: yes/no";
+
+  movieInfoDiv.append(h3, dateP, ratingP, watchedP);
+
+  movieDiv.append(img, movieInfoDiv);
+
+  listContainer.append(movieDiv);
+}
